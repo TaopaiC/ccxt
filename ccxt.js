c20a94e0a9990c89f7facbcf749cbcbcf0f2c770
TaopaiC <pctao.blog@pctao.org>
2020-06-09 03:54:19 +0800

    FEATURE: import max.js from max-191223

--- ccxt.js
+++ ccxt.js
@@ -136,6 +136,7 @@ const exchanges = {
     'livecoin':                require ('./js/livecoin.js'),
     'luno':                    require ('./js/luno.js'),
     'lykke':                   require ('./js/lykke.js'),
+    'max':                     require ('./js/max.js'),
     'mercado':                 require ('./js/mercado.js'),
     'mixcoins':                require ('./js/mixcoins.js'),
     'novadax':                 require ('./js/novadax.js'),
